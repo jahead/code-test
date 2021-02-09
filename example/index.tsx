bc2a2fd7c3b10b75f1a232634c36c139a25d9808
Joshua Head <joshua.head@outlook.com>
2021-02-09 18:52:53 +1100

    refactor: moved ControlItemCard to old file

--- example/index.tsx
+++ example/index.tsx
@@ -1,7 +1,8 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Header, ItemGrid, ItemCard, Footer, ItemCardProps } from '../src';
+import { Header, ItemGrid, Footer } from '../src';
+import { ControlledItemCard } from './containers/ControlledItemCard';
 
 import './assets/index.css';
 
@@ -66,31 +67,6 @@ const args = {
     { label: 'Terms & Conditions', href: '/tos' },
     { label: 'contact', href: '/contact' },
   ],
-};
-
-/**
- * exponential moving average using an N of 10
- */
-const N = 10; // Number of samples we want to average over
-const calcMovingAverage = (previousValue: number, currentValue: number) => {
-  let newAvg = previousValue - previousValue / N;
-  newAvg = newAvg + currentValue / N;
-  return newAvg;
-};
-
-const ControlledItemCard = (props: Omit<ItemCardProps, 'onClick'>) => {
-  const [rating, setRating] = React.useState(props.rating);
-  return (
-    <ItemCard
-      {...props}
-      rating={rating}
-      onClick={(value, old) => {
-        old === undefined || old === null
-          ? setRating(value)
-          : setRating(calcMovingAverage(old, value));
-      }}
-    />
-  );
 };
 
 const App = () => {
