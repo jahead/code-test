386990746f7a885121233b8410b907060905cc98
Joshua Head <joshua.head@outlook.com>
2021-02-09 18:20:14 +1100

    refractor(RatingBar): code cleanup

--- src/components/RatingsBar/RatingBar.tsx
+++ src/components/RatingsBar/RatingBar.tsx
@@ -2,19 +2,22 @@ import React from 'react';
 import { StarItem } from './StarItem';
 import { RatingsCounterText } from './RatingsCounterText';
 
+const noop = () => {};
+
 export type RatingsBarProps = {
   score?: number | null;
   onClick?: (value: number) => void;
 };
 
 const scoreThresholds = [1, 2, 3, 4, 5];
-const noop = () => {};
+
+const style = { display: 'flex', alignSelf: 'start' };
 
 export const RatingsBar = (props: RatingsBarProps) => {
   const score = props.score ? props.score + 0.009 : 0; // Because JS has a bad round function.
   const onClick = props.onClick ?? noop;
   return (
-    <div style={{ display: 'flex', alignSelf: 'start' }}>
+    <div style={style}>
       {scoreThresholds.map(index => (
         <StarItem
           key={index.toString()}
